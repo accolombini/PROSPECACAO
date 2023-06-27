02ce92c60266239d078fde4a8e44a13f7522b0ce
UFF <accolombini@id.uff.br>
2023-06-27 14:50:04 -0300

    Fase de limpeza de dados e detecção de ouliers ok

--- np_rastreia_falta.py
+++ np_rastreia_falta.py
@@ -15,7 +15,14 @@ def load_data(filename):
     df = pd.read_csv(filename)
     df_normal = df[df['role'] == 'normal'].copy()
     df_test = df[df['role'] == 'test-0'].copy()
+    
+    # Remover as colunas "role" e "offset_seconds"
+    columns_to_remove = ['role', 'offset_seconds']
+    df_normal = df_normal.drop(columns=columns_to_remove)
+    df_test = df_test.drop(columns=columns_to_remove)
+    
     return df_normal, df_test
+
 
 def calculate_missing_data(df):
     total_variables = len(df.columns)
@@ -25,21 +32,23 @@ def calculate_missing_data(df):
 def find_outliers(df):
     outliers_report = []
     for col in df.columns:
-        if df[col].dtype in ['int64', 'float64']:  # Check if the column is numeric
+        if df[col].dtype in ['int64', 'float64']:
             z_scores = zscore(df[col])
             outliers_indices = df.index[abs(z_scores) > 3].tolist()
             if outliers_indices:
-                outliers_report.append({'Variável': col, 'Índices_dos_Outliers': outliers_indices, 'Total_de_Outliers': len(outliers_indices)})
+                outliers_report.append({'Variável': col, 'Índices dos Outliers': outliers_indices, 'Total de Outliers': len(outliers_indices)})
     return pd.DataFrame(outliers_report)
+
 
 def check_possible_fault(df):
     possible_fault_report = []
     for col in df.columns:
-        if df[col].dtype in ['int64', 'float64']:  # Check if the column is numeric
+        if df[col].dtype in ['int64', 'float64']:
             outliers_indices = df.index[df[col] != 0].tolist()
             if outliers_indices:
-                possible_fault_report.append({'Variável': col, 'Índices_dos_Registros_com_Possível_Falta': outliers_indices})
+                possible_fault_report.append({'Variável': col, 'Índices dos Registros com Possível Falta': outliers_indices})
     return pd.DataFrame(possible_fault_report)
+
 
 def save_to_excel(missing_data_normal, missing_data_test, outliers_report_normal, outliers_report_test, possible_fault_report_normal, possible_fault_report_test, output_file):
     # Create a new workbook
@@ -70,7 +79,6 @@ def save_to_excel(missing_data_normal, missing_data_test, outliers_report_normal
     ws_outliers_report_normal.append(outliers_report_normal_columns)
     ws_outliers_report_test.append(outliers_report_test_columns)
     ws_possible_fault_normal.append(possible_fault_report_normal_columns)
-    ws_possible_fault_test.append(possible_fault_report_test_columns)
 
     # Adding data to worksheets
     for row in dataframe_to_rows(missing_data_normal, index=False, header=True):
@@ -79,30 +87,36 @@ def save_to_excel(missing_data_normal, missing_data_test, outliers_report_normal
     for row in dataframe_to_rows(missing_data_test, index=False, header=True):
         ws_missing_data_test.append(row)
 
-    for row in dataframe_to_rows(outliers_report_normal, index=False, header=True):
-        ws_outliers_report_normal.append(row)
+    for _, row in outliers_report_normal.iterrows():
+        indices = ', '.join(map(str, row['Índices dos Outliers']))
+        ws_outliers_report_normal.append([row['Variável'], indices, row['Total de Outliers']])
 
-    for row in dataframe_to_rows(outliers_report_test, index=False, header=True):
-        ws_outliers_report_test.append(row)
+    for _, row in outliers_report_test.iterrows():
+        indices = ', '.join(map(str, row['Índices dos Outliers']))
+        ws_outliers_report_test.append([row['Variável'], indices, row['Total de Outliers']])
 
-    for row in dataframe_to_rows(possible_fault_report_normal, index=False, header=True):
-        ws_possible_fault_normal.append(row)
+    for _, row in possible_fault_report_normal.iterrows():
+        indices = ', '.join(map(str, row['Índices dos Registros com Possível Falta']))
+        ws_possible_fault_normal.append([row['Variável'], indices])
 
-    for row in dataframe_to_rows(possible_fault_report_test, index=False, header=True):
-        ws_possible_fault_test.append(row)
+    for _, row in possible_fault_report_test.iterrows():
+        indices = ', '.join(map(str, row['Índices dos Registros com Possível Falta']))
+        ws_possible_fault_test.append([row['Variável'], indices])
 
     # Save the workbook
     wb.save(output_file)
 
-    #-------------------------------CHAMADA DAS FUNÇÕES
+# Chamada das funções para obter os dados
+df_normal, df_test = load_data('DADOS/Adendo A.2_Conjunto de Dados_DataSet.csv')
+missing_data_normal = calculate_missing_data(df_normal)
+missing_data_test = calculate_missing_data(df_test)
+outliers_report_normal = find_outliers(df_normal)
+outliers_report_test = find_outliers(df_test)
+possible_fault_report_normal = check_possible_fault(df_normal)
+possible_fault_report_test = check_possible_fault(df_test)
 
-    # Chamada das funções para obter os dados
-    missing_data_normal, missing_data_test = calculate_missing_data(df_normal), calculate_missing_data(df_test)
-    outliers_report_normal, outliers_report_test = find_outliers(df_normal), find_outliers(df_test)
-    possible_fault_report_normal, possible_fault_report_test = check_possible_fault(df_normal),     check_possible_fault(df_test)
+# Nome do arquivo de saída
+output_file = 'DADOS/POSS_FALT.xlsx'
 
-    # Nome do arquivo de saída
-    output_file = 'POSS_FALT.xlsx'
-
-    # Salvar os dados no arquivo Excel
-    save_to_excel(missing_data_normal, missing_data_test, outliers_report_normal, outliers_report_test,     possible_fault_report_normal, possible_fault_report_test, output_file)
+# Salvar os dados no arquivo Excel
+save_to_excel(missing_data_normal, missing_data_test, outliers_report_normal, outliers_report_test, possible_fault_report_normal, possible_fault_report_test, output_file)
